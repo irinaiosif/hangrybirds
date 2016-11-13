2944c00379f30cdecdac8c6194d3a917a534bed2
vlad vartolomei <vlad.vartolomei@gmail.com>
2016-11-13 14:07:52 +0200

    fixed movement

--- Assets/BugController.cs
+++ Assets/BugController.cs
@@ -3,36 +3,37 @@ using System.Collections;
 
 public class BugController : MonoBehaviour {
 
+    float delta = 0.02f;
+
 	// Use this for initialization
 	void Start () {
-        print("zuzuz");
-	
+        print("BUG::Movement initiated");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A) )
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A) )
         {
             Vector3 position = this.transform.position;
-            position.x--;
+            position.x-= delta;
             this.transform.position = position;
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
             Vector3 position = this.transform.position;
-            position.x++;
+            position.x+= delta;
             this.transform.position = position;
         }
-        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
         {
             Vector3 position = this.transform.position;
-            position.y++;
+            position.y+= delta;
             this.transform.position = position;
         }
-        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
         {
             Vector3 position = this.transform.position;
-            position.y--;
+            position.y-= delta;
             this.transform.position = position;
         }
     }
